667c9a26770f3514eea505def34d0f67332d71d7
MukterHossain <mukter3075@gmail.com>
2024-12-19 20:04:23 +0600

    added checkoutFrom setup

--- src/components/form/CheckoutForm.jsx
+++ src/components/form/CheckoutForm.jsx
@@ -1,15 +1,39 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import PropTypes from "prop-types";
+import {ImSpinner9} from 'react-icons/im'
+import "./CheckoutForm.css";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "../../hooks/useAuth";
 
-import './CheckoutForm.css';
-
-const CheckoutForm = () => {
+const CheckoutForm = ({ closeModal, bookingInfo }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [clientSecret, setClientSecret] = useState();
+  const [cardError, setCardError] = useState("");
+  const [processing, setProcessing] = useState(false);
+
+  useEffect(() => {
+    // fetch client secret
+    if (bookingInfo?.price && bookingInfo?.price > 1) {
+      getClientSecret({ price: bookingInfo?.price });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bookingInfo?.price]);
+
+  //get clientSecret
+  const getClientSecret = async (price) => {
+    const { data } = await axiosSecure.post(`/create-payment-intent`, price);
+    console.log("Client Secret data", data);
+    setClientSecret(data.clientSecret);
+  };
 
   const handleSubmit = async (event) => {
     // Block native form submission.
     event.preventDefault();
-
+    setProcessing(true);
     if (!stripe || !elements) {
       // Stripe.js has not loaded yet. Make sure to disable
       // form submission until Stripe.js has loaded.
@@ -26,41 +50,98 @@ const CheckoutForm = () => {
     }
 
     // Use your card Element with other Stripe.js APIs
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type: 'card',
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
       card,
     });
 
     if (error) {
-      console.log('[error]', error);
+      console.log("[error]", error);
+      setCardError(error.message);
+      setProcessing(false)
+      return;
     } else {
-      console.log('[PaymentMethod]', paymentMethod);
+      console.log("[PaymentMethod]", paymentMethod);
+      setCardError("");
     }
+    // confirm payment
+    const { error: confirmError, paymentIntent } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email,
+            name: user?.displayName,
+          },
+        },
+      });
+      if(confirmError){
+        console.log(confirmError)
+        setCardError(confirmError.message)
+        setProcessing(false)
+        return
+      }
+      if (paymentIntent.status === 'succeeded') {
+        console.log(paymentIntent)
+        // 1. Create payment info object
+        const paymentInfo = {
+          ...bookingInfo,
+        //   roomId: bookingInfo._id,
+          transactionId: paymentIntent.id,
+          date: new Date(),
+        }
+        console.log(paymentInfo)
+        // 2. save payment info in booking collection (db)
+        // 3. change room status to booked in db
+    }
+    setProcessing(false)
+      
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <div className="flex mt-2 justify-around">
+          <button
+            type="submit"
+            disabled={!stripe || !clientSecret || processing}
+            className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+          >
+            {processing ? <ImSpinner9 size={24} className="animate-spin m-auto"></ImSpinner9> : `Pay ${bookingInfo?.price}`}
+          </button>
+          <button
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+            onClick={closeModal}
+          >
+            Cencel
+          </button>
+        </div>
+      </form>
+      {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+    </>
   );
+};
+CheckoutForm.propTypes = {
+  bookingInfo: PropTypes.object,
+  closeModal: PropTypes.func,
+  // refetch: PropTypes.func,
 };
 
 export default CheckoutForm;
